3a346aefe636cd68fff56843d9404888ed3ee79b
xzcbjtu <953955314@qq.com>
2016-05-12 11:07:53 +0800

    Initial commit

---
+++ app/src/test/java/bjtu/edu/cn/foodtoslim/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package bjtu.edu.cn.foodtoslim;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
